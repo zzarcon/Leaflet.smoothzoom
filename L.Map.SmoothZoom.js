9d51997efb1b563548a96ad00bfcddfbd728a7d5
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-05-24 16:05:11 +0200

    es5

--- L.Map.SmoothZoom.js
+++ L.Map.SmoothZoom.js
@@ -2,12 +2,12 @@ L.Map.SmoothZoom = L.Handler.extend({
   smoothZoomDelay: 1000,
   isThrottling: false,
 
-  addHooks: function () {
+  addHooks: function() {
     this._map.scrollWheelZoom.removeHooks();
     L.DomEvent.on(this._map._container, 'mousewheel', this.onMouseWheel, this);
   },
 
-  removeHooks: function () {
+  removeHooks: function() {
     L.DomEvent.off(this._map._container, 'mousewheel', this.onMouseWheel, this);
   },
 
@@ -20,20 +20,20 @@ L.Map.SmoothZoom = L.Handler.extend({
     this.isThrottling = true;
     this.setSmoothZoom(e);
 
-    setTimeout(() => {
+    setTimeout(function() {
       this.isThrottling = false;
-    }, this.smoothZoomDelay);
+    }.bind(this), this.smoothZoomDelay);
   },
 
-  setSmoothZoom(e) {
-    const delta = L.DomEvent.getWheelDelta(e);
-    const map = this._map;
+  setSmoothZoom: function(e) {
+    var delta = L.DomEvent.getWheelDelta(e);
+    var map = this._map;
 
     if (!delta) return;
 
-    const newZoom = delta > 0 ? 1 : -1;
+    var newZoom = delta > 0 ? 1 : -1;
 
-    map.setZoom(map.getZoom() + newZoom)
+    map.setZoom(map.getZoom() + newZoom);
   }
 });
 
